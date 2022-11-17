54c573256a782d6fce1fd90c33f828f85e7cebaf
myyna <myyna@DESKTOP-95NQD5D.wifi.uc.edu>
2022-11-17 08:38:31 -0500

    Add main package

---
+++ LC415/mainPackage/Main.py
@@ -0,0 +1,9 @@
+# Main.py
+
+from solutionPackage.Solution import *
+# This is our entry point to test code
+# We will test the solution class for leetcode problem 415
+# The solution has been provided by Bill
+
+# Instantiate and object of type Solution
+mySolution = Solution()
